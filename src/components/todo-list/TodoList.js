c74d4bd550c7c6465ff314fa41eed0bc0998d82a
Roy Montoya <roymontoya89@gmail.com>
2018-07-11 11:47:24 -0700

    added functionallity to check a todo

--- src/components/todo-list/TodoList.js
+++ src/components/todo-list/TodoList.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchTodos} from '../../reducers/todo'
+import {fetchTodos, toggleTodo} from '../../reducers/todo'
 
-const TodoItem = ({id, name, isComplete}) =>(<li>
-  <input type="checkbox" defaultChecked={isComplete}/>
+const TodoItem = ({id, name, isComplete, toggleTodo}) =>(<li>
+  <input type="checkbox"
+    checked={isComplete}
+    onChange={() => toggleTodo(id)}/>
   {name}
 </li>)
 
@@ -19,7 +21,7 @@ class TodoList extends Component{
     return (<div className="todo-list">
       <ul>
         {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} {...todo}/>
+          <TodoItem key={todo.id} toggleTodo={this.props.toggleTodo} {...todo}/>
         ))}
       </ul>
     </div>)
@@ -27,5 +29,5 @@ class TodoList extends Component{
 }
 
 export default connect(
-  (state) =>({todos: state.todo.todos}), {fetchTodos}
+  (state) =>({todos: state.todo.todos}), {fetchTodos, toggleTodo}
 )(TodoList)
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -9,13 +9,14 @@ const initState = {
 
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 export const TODO_ADD = 'TODO_ADD'
-export  const TODO_LOAD = 'TODOS_LOAD'
-
+export const TODO_LOAD = 'TODOS_LOAD'
+export const TODO_REPLACE = 'TODOS_REPLACE'
 
 
 export const loadTodos = (todos) => ({type: TODO_LOAD, payload: todos})
 export const addTodo = (todo) => ({type: TODO_ADD, payload:todo})
 export const updateCurrent = (val) => ({type:CURRENT_UPDATE, payload:val})
+export const replaceTodo = (todo) => ({type:TODO_REPLACE, payload:todo})
 export const fetchTodos = () => {
   return (dispatch) => {
     dispatch(showMessage('Loading Todos'))
@@ -31,6 +32,17 @@ export const saveTodo = (name) =>{
   }
 }
 
+export const toggleTodo = (id) => {
+  return (dispatch, getState) => {
+    dispatch(showMessage('Saving Todo update'))
+    const {todos} = getState().todo
+    const todo = todos.find(t => t.id === id)
+    const toggled = { ...todo, isComplete: !todo.isComplete}
+    updateTodo(toggled)
+      .then(res => dispatch(replaceTodo(res)))
+  }
+}
+
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
@@ -39,6 +51,11 @@ export default (state = initState, action) => {
       return {...state, todos: action.payload}
     case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
+    case TODO_REPLACE:
+      return {...state,
+        todos: state.todos.map(t => t.id === action.payload.id
+          ? action.payload
+          : t)}
     default:
         return state;
   }
